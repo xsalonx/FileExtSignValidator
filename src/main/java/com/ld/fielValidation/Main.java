d2ec5f4d0d5f3f2c6072c6ecfbf82b0b84e65b62
xsalonx <lukasz.dubiel3@gmail.com>
2021-07-19 07:11:15 +0200

    main

--- src/main/java/com/ld/fielValidation/Main.java
+++ src/main/java/com/ld/fielValidation/Main.java
@@ -1,4 +1,14 @@
 package com.ld.fielValidation;
 
 public class Main {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("No input");
+        }
+
+        for (String s : args) {
+            System.out.println(args);
+        }
+    }
 }
